0e630eb090fad0cf31adb291635d6e25de172bdd
Daniel Scalzi <d_scalzi@yahoo.com>
2018-11-25 21:59:42 -0500

    Fix tab issue (not fixed by linter?)

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Requirements
 const {app, BrowserWindow, ipcMain} = require('electron')
-const Menu							= require('electron').Menu
+const Menu                          = require('electron').Menu
 const autoUpdater                   = require('electron-updater').autoUpdater
 const ejse                          = require('ejs-electron')
 const fs                            = require('fs')
