b1edcf3cd609350ba2c7d706d6a72e4803a744b7
MastermDEV <31785253+MastermDEV@users.noreply.github.com>
2020-06-18 18:16:45 -0700

    Add files via upload

--- build.js
+++ build.js
@@ -18,10 +18,10 @@ function getCurrentPlatform(){
 builder.build({
     targets: (process.argv[2] != null && Platform[process.argv[2]] != null ? Platform[process.argv[2]] : getCurrentPlatform()).createTarget(),
     config: {
-        appId: 'helioslauncher',
-        productName: 'Helios Launcher',
+        appId: 'nemesismclauncher',
+        productName: 'NemesisMC Launcher',
         artifactName: '${productName}-setup-${version}.${ext}',
-        copyright: 'Copyright © 2018-2020 Daniel Scalzi',
+        copyright: 'Copyright © 2020 NemesisMC',
         directories: {
             buildResources: 'build',
             output: 'dist'
@@ -46,10 +46,10 @@ builder.build({
         },
         linux: {
             target: 'AppImage',
-            maintainer: 'Daniel Scalzi',
-            vendor: 'Daniel Scalzi',
-            synopsis: 'Modded Minecraft Launcher',
-            description: 'Custom launcher which allows users to join modded servers. All mods, configurations, and updates are handled automatically.',
+            maintainer: 'MastermDEV, Daniel Scalzi',
+            vendor: 'NemesisMC',
+            synopsis: 'Offical NemesisMC Launcher',
+            description: 'Custom launcher which allows users to join NemesisMC.',
             category: 'Game'
         },
         compression: 'maximum',
