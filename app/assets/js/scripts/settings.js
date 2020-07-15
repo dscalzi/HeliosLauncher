fcef514ca141f0872cd77219379f69a9ddec0b76
Michael Anderson <yadoneson175@gmail.com>
2020-07-14 19:37:21 -0600

    update vars

--- app/assets/js/scripts/settings.js
+++ app/assets/js/scripts/settings.js
@@ -1219,7 +1219,7 @@ function populateAboutVersionInformation(){
  */
 function populateReleaseNotes(){
     $.ajax({
-        url: 'https://github.com/dscalzi/HeliosLauncher/releases.atom',
+        url: 'https://github.com/ArdaCraft/ArdaCraftLauncher/releases.atom',
         success: (data) => {
             const version = 'v' + remote.app.getVersion()
             const entries = $(data).find('entry')
