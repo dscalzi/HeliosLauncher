0a39fcef623e16717053ca83453f445422a786e9
Michael Anderson <yadoneson175@gmail.com>
2020-07-15 23:03:24 -0600

    change distro url

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -537,7 +537,7 @@ exports.pullRemote = function(){
         return exports.pullLocal()
     }
     return new Promise((resolve, reject) => {
-        const distroURL = 'https://ardacraft.github.io/NewLauncher/distribution.json'
+        const distroURL = 'https://ardacraft.github.io/launcher/distribution.json'
         //const distroURL = 'https://gist.githubusercontent.com/dscalzi/53b1ba7a11d26a5c353f9d5ae484b71b/raw/'
         const opts = {
             url: distroURL,
