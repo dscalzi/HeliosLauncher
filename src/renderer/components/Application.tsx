043f85c0dc083bf302e71b80c8ba2d7a75e60e35
Daniel Scalzi <d_scalzi@yahoo.com>
2020-09-13 19:05:29 -0400

    Convert generic overlay css from px to rem. (#109)

--- src/renderer/components/Application.tsx
+++ src/renderer/components/Application.tsx
@@ -303,11 +303,11 @@ class Application extends React.Component<InternalApplicationProps, ApplicationS
                     //         console.log(serverStatus)
                     //     }
                     // })
-                    // this.props.pushGenericOverlay({
-                    //     title: 'Test Title 2',
-                    //     description: 'Test Description',
-                    //     dismissible: true
-                    // })
+                    this.props.pushGenericOverlay({
+                        title: 'Test Title 2',
+                        description: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.',
+                        dismissible: true
+                    })
                     // this.props.pushGenericOverlay({
                     //     title: 'Test Title IMPORTANT',
                     //     description: 'Test Description',
