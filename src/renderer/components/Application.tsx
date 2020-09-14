6a04ef0f62dad6db972211e4ede37508a6a8bb8a
Daniel Scalzi <d_scalzi@yahoo.com>
2020-09-13 22:18:08 -0400

    Move landing media buttons to their own component.
    The media buttons can either open a link or perform an action.
    TODO: Convert CSS from px to rem.

--- src/renderer/components/Application.tsx
+++ src/renderer/components/Application.tsx
@@ -288,6 +288,7 @@ class Application extends React.Component<InternalApplicationProps, ApplicationS
 
             // Pick a background id.
             this.bkid = Math.floor((Math.random() * (await readdir(join(__static, 'images', 'backgrounds'))).length))
+            this.bkid = 3 // TEMP
 
             const endLoad = () => {
                 // TODO determine correct view
@@ -310,11 +311,11 @@ class Application extends React.Component<InternalApplicationProps, ApplicationS
                     //         console.log(serverStatus)
                     //     }
                     // })
-                    this.props.pushGenericOverlay({
-                        title: 'Test Title 2',
-                        description: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.',
-                        dismissible: true
-                    })
+                    // this.props.pushGenericOverlay({
+                    //     title: 'Test Title 2',
+                    //     description: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.',
+                    //     dismissible: true
+                    // })
                     // this.props.pushGenericOverlay({
                     //     title: 'Test Title IMPORTANT',
                     //     description: 'Test Description',
