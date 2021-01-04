10c8b47ec155463573bef9af25bfe4014ffa1775
DevLarge <DevLarge@users.noreply.github.com>
2021-01-04 15:30:31 +0100

    Updates 1 safe

--- build.js
+++ build.js
@@ -18,10 +18,10 @@ function getCurrentPlatform(){
 builder.build({
     targets: (process.argv[2] != null && Platform[process.argv[2]] != null ? Platform[process.argv[2]] : getCurrentPlatform()).createTarget(),
     config: {
-        appId: 'helioslauncher',
-        productName: 'Helios Launcher',
+        appId: 'stellarnetworklauncher',
+        productName: 'Stellar Network Launcher',
         artifactName: '${productName}-setup-${version}.${ext}',
-        copyright: 'Copyright © 2018-2020 Daniel Scalzi',
+        copyright: 'Copyright © 2018-2021 Stellar Network',
         directories: {
             buildResources: 'build',
             output: 'dist'
@@ -46,10 +46,10 @@ builder.build({
         },
         linux: {
             target: 'AppImage',
-            maintainer: 'Daniel Scalzi',
-            vendor: 'Daniel Scalzi',
-            synopsis: 'Modded Minecraft Launcher',
-            description: 'Custom launcher which allows users to join modded servers. All mods, configurations, and updates are handled automatically.',
+            maintainer: 'Stellar Network',
+            vendor: 'Stellar Network',
+            synopsis: 'Modded Minecraft Launcher for Stellar Network',
+            description: 'Custom launcher which allows users to join modded servers from Stellar.',
             category: 'Game'
         },
         compression: 'maximum',
