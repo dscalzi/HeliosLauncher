6b15c2f7b54b0e2a99319d413318c36588e3d3ce
DevLarge <DevLarge@users.noreply.github.com>
2021-01-06 14:07:20 +0100

    Ups

--- app/assets/js/processbuilder.js
+++ app/assets/js/processbuilder.js
@@ -483,7 +483,7 @@ class ProcessBuilder {
                             val = args[i].replace(argDiscovery, tempNativePath)
                             break
                         case 'launcher_name':
-                            val = args[i].replace(argDiscovery, 'Stellar-Network-Launcher')
+                            val = args[i].replace(argDiscovery, 'StellarNetworkLauncher')
                             break
                         case 'launcher_version':
                             val = args[i].replace(argDiscovery, this.launcherVersion)
