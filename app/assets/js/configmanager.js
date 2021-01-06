3691d0ff5fd3cbb666ab3e2f8cf79704e49d6ad9
DevLarge <DevLarge@users.noreply.github.com>
2021-01-06 12:02:35 +0100

    Update configmanager.js

--- app/assets/js/configmanager.js
+++ app/assets/js/configmanager.js
@@ -6,7 +6,7 @@ const logger = require('./loggerutil')('%c[ConfigManager]', 'color: #a02d2a; fon
 
 const sysRoot = process.env.APPDATA || (process.platform == 'darwin' ? process.env.HOME + '/Library/Application Support' : process.env.HOME)
 // TODO change
-const dataPath = path.join(sysRoot, '.helioslauncher')
+const dataPath = path.join(sysRoot, '.stellarnetworklauncher')
 
 // Forked processes do not have access to electron, so we have this workaround.
 const launcherDir = process.env.CONFIG_DIRECT_PATH || require('electron').remote.app.getPath('userData')
