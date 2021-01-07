e3699f53de0f9cd8338a3de3d308f662f7afabfe
DevLarge <DevLarge@users.noreply.github.com>
2021-01-07 17:37:01 +0100

    Update build.js

--- build.js
+++ build.js
@@ -20,7 +20,8 @@ builder.build({
     config: {
         appId: 'stellarnetworklauncher',
         productName: 'Stellar Network Launcher',
-        artifactName: '${productName}-setup-1.0.0.1.${ext}',
+        // artifactName: '${productName}-setup-${version}.${ext}',
+        artifactName: '${productName}-setup-${version}.${ext}',
         copyright: 'Copyright © 2018-2021 Stellar Network',
         directories: {
             buildResources: 'build',
