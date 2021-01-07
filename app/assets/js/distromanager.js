9f74ac65a6991facc028b0aef74e471686ed8cd8
DrDeee <63226814+DrDeee@users.noreply.github.com>
2021-01-07 22:20:25 +0100

    Update pullRemove for testing mode

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -538,9 +538,10 @@ exports.pullRemote = function(){
     }
     return new Promise((resolve, reject) => {
         const distroURL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
+        const testDistroURL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
         //const distroURL = 'https://gist.githubusercontent.com/dscalzi/53b1ba7a11d26a5c353f9d5ae484b71b/raw/'
         const opts = {
-            url: distroURL,
+            url: ConfigManager.isTesting()? testDistroURL : distroURL,
             timeout: 2500
         }
         const distroDest = path.join(ConfigManager.getLauncherDirectory(), 'distribution.json')
