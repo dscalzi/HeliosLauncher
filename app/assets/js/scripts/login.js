a81923eaccd65ba0fc88809c121affce3fc865f9
Freshmilkymilk <fresh@freshy.me>
2021-02-01 13:56:24 -0700

    Update launcher name to match ours

--- app/assets/js/scripts/login.js
+++ app/assets/js/scripts/login.js
@@ -321,7 +321,7 @@ ipcRenderer.on('MSALoginWindowReply', (event, ...args) => {
         let errorDesc = queryMap.get('error_description')
         if(error === 'access_denied'){
             error = 'ERRPR'
-            errorDesc = 'To use the NexusLauncher, you must agree to the required permissions! Otherwise you can\'t use this launcher with Microsoft accounts.<br><br>Despite agreeing to the permissions you don\'t give us the possibility to do anything with your account, because all data will always be sent back to you (the launcher) IMMEDIATELY and WITHOUT WAY.'
+            errorDesc = 'To use the ArdaCraftLauncher, you must agree to the required permissions! Otherwise you can\'t use this launcher with Microsoft accounts.<br><br>Despite agreeing to the permissions you don\'t give us the possibility to do anything with your account, because all data will always be sent back to you (the launcher) IMMEDIATELY and WITHOUT WAY.'
         }        
         setOverlayContent(error, errorDesc, 'OK')
         setOverlayHandler(() => {
