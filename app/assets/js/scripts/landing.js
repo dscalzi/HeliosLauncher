9e16c2a7906f2faa5eae0662d308d216ac05ddb9
Michael Anderson <mikeandeeson@gmail.com>
2021-03-28 16:29:28 -0600

    Update landing.js

--- app/assets/js/scripts/landing.js
+++ app/assets/js/scripts/landing.js
@@ -681,9 +681,9 @@ function dlAsync(login = true){
                 const gameStateChange = function(data){
                     data = data.trim()
                     if(SERVER_JOINED_REGEX.test(data)){
-                        DiscordWrapper.updateDetails('Exploring the Realm!')
+                        DiscordWrapper.updateDetails('Exploring Middle-earth!')
                     } else if(GAME_JOINED_REGEX.test(data)){
-                        DiscordWrapper.updateDetails('Sailing to Westeros!')
+                        DiscordWrapper.updateDetails('Sailing to Middle-earth!')
                     }
                 }
 
