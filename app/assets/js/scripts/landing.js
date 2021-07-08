73136b7b35bff2f10d77310f06838c1d040aa5f8
Peter <omgpieisawesome@hotmail.com>
2021-07-08 23:27:46 +0100

    Update landing.js
    This changes the Skin/Avatar server displayed on the landing page to one that displays the hat layer which then features the skin correctly.

--- app/assets/js/scripts/landing.js
+++ app/assets/js/scripts/landing.js
@@ -130,7 +130,7 @@ function updateSelectedAccount(authUser){
             username = authUser.displayName
         }
         if(authUser.uuid != null){
-            document.getElementById('avatarContainer').style.backgroundImage = `url('https://crafatar.com/renders/body/${authUser.uuid}')`
+            document.getElementById('avatarContainer').style.backgroundImage = `url('https://mc-heads.net/body/$npm{authUser.uuid}/right')`
         }
     }
     user_text.innerHTML = username
