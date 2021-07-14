93db220bcc466fd1b463de56107c10b8b3493b7a
Thomas Apter <thomasapter@btinternet.com>
2021-07-14 22:29:40 +0100

    Fix image not appearing due to url
    Images will now appear in news feed - is most likely a single issue fix, will update code if problem persists.

--- app/assets/js/scripts/landing.js
+++ app/assets/js/scripts/landing.js
@@ -1262,7 +1262,8 @@ function loadNews(){
                     let regex = /src="(?!http:\/\/|https:\/\/)(.+?)"/g
                     let matches
                     while((matches = regex.exec(content))){
-                        content = content.replace(`"${matches[1]}"`, `"${newsHost + matches[1]}"`)
+                        // Fix //news issue - Probably only fixes this situation, if problem persists will make more robust.
+                        content = content.replace(`"${matches[1]}"`, `"https:${matches[1]}"`)
                     }
 
                     let link   = el.find('link').text()
