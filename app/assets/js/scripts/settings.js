54e657275466294986b6c4977941396e32c03eba
Peter <omgpieisawesome@hotmail.com>
2021-07-19 16:08:55 +0100

    Use MCHeads instead of Crafatar (#181)
    * Update landing.js
    
    This changes the Skin/Avatar server displayed on the landing page to one that displays the hat layer which then features the skin correctly.
    
    * Create landing.js
    
    This changes the Skin/Avatar server displayed on the landing page to one that displays the hat layer which then features the skin correctly.
    
    * Updated remaining Crafatar URLS
    
    Changed Crafatar URLS to matching mc-heads url.

--- app/assets/js/scripts/settings.js
+++ app/assets/js/scripts/settings.js
@@ -444,7 +444,7 @@ function populateAuthAccounts(){
         const acc = authAccounts[val]
         authAccountStr += `<div class="settingsAuthAccount" uuid="${acc.uuid}">
             <div class="settingsAuthAccountLeft">
-                <img class="settingsAuthAccountImage" alt="${acc.displayName}" src="https://crafatar.com/renders/body/${acc.uuid}?scale=3&default=MHF_Steve&overlay">
+                <img class="settingsAuthAccountImage" alt="${acc.displayName}" src="https://mc-heads.net/body/${acc.uuid}/60">
             </div>
             <div class="settingsAuthAccountRight">
                 <div class="settingsAuthAccountDetails">
