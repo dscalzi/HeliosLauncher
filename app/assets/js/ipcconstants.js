89753f2bad0d0b24f7feb937e2738876d42fafe1
SoraxDubbing <52173496+Sorax5@users.noreply.github.com>
2022-02-22 16:47:02 +0100

    Update ipcconstants.js

--- app/assets/js/ipcconstants.js
+++ app/assets/js/ipcconstants.js
@@ -1,7 +1,7 @@
 // NOTE FOR THIRD-PARTY
 // REPLACE THIS CLIENT ID WITH YOUR APPLICATION ID.
 // SEE https://github.com/dscalzi/HeliosLauncher/blob/master/docs/MicrosoftAuth.md
-exports.AZURE_CLIENT_ID = '1ce6e35a-126f-48fd-97fb-54d143ac6d45'
+exports.AZURE_CLIENT_ID = '681c8525-6c74-411a-be03-8e367f2b0821'
 // SEE NOTE ABOVE.
 
 
