bf3d2fbd8dfc1a588b2cf45d895922d420435345
SoraxDubbing <52173496+Sorax5@users.noreply.github.com>
2022-02-27 18:19:14 +0100

    test auto Update

--- app/assets/js/scripts/uicore.js
+++ app/assets/js/scripts/uicore.js
@@ -50,7 +50,7 @@ if(!isDev){
                 loggerAutoUpdaterSuccess.log('New update available', info.version)
                 
                 if(process.platform === 'darwin'){
-                    info.darwindownload = `https://github.com/dscalzi/HeliosLauncher/releases/download/v${info.version}/Helios-Launcher-setup-${info.version}${process.arch === 'arm64' ? '-arm64' : '-x64'}.dmg`
+                    info.darwindownload = `https://github.com/Sorax5/RoadToNincraftStudioLeblanc-Launcher/releases/download/v${info.version}/RoadToNincraft-Launcher-setup-${info.version}${process.arch === 'arm64' ? '-arm64' : '-x64'}.dmg`
                     showUpdateUI(info)
                 }
                 
