fe1ad28b4a0948d271314cde07c6d95f05468f45
SoraxDubbing <52173496+Sorax5@users.noreply.github.com>
2022-02-27 18:39:43 +0100

    AutoTest

--- app/assets/js/scripts/settings.js
+++ app/assets/js/scripts/settings.js
@@ -1403,7 +1403,7 @@ function populateAboutVersionInformation(){
  */
 function populateReleaseNotes(){
     $.ajax({
-        url: 'https://github.com/dscalzi/HeliosLauncher/releases.atom',
+        url: 'https://github.com/luki-39/LukiEnLiveLauncher/releases.atom',
         success: (data) => {
             const version = 'v' + remote.app.getVersion()
             const entries = $(data).find('entry')
