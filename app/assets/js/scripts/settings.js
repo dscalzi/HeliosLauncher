a9c87e011cdc96753c332f5422c37cb1dfb23daf
Yoann Brigant <yoann.2001@hotmail.fr>
2022-08-18 09:41:31 +0200

    server update

--- app/assets/js/scripts/settings.js
+++ app/assets/js/scripts/settings.js
@@ -1407,7 +1407,7 @@ function populateAboutVersionInformation(){
  */
 function populateReleaseNotes(){
     $.ajax({
-        url: 'https://github.com/dscalzi/HeliosLauncher/releases.atom',
+        url: 'https://github.com/ElBrigos/TabernaLauncher/releases.atom',
         success: (data) => {
             const version = 'v' + remote.app.getVersion()
             const entries = $(data).find('entry')
