ee96980dee874375c021cd0a8e6aa986ff3d499c
Daniel Scalzi <d_scalzi@yahoo.com>
2023-03-20 21:24:56 -0400

    New distribution url.

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -2,7 +2,9 @@ const { DistributionAPI } = require('helios-core/common')
 
 const ConfigManager = require('./configmanager')
 
-exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
+// Old WesterosCraft url.
+// exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
+exports.REMOTE_DISTRO_URL = 'https://helios-files.geekcorner.eu.org/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
