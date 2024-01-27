2643830278de44954e5298dca86d0d7dd6c2bb85
DrEmixam <maxime@melinon.com>
2024-01-27 19:46:29 +0100

    Changements sur les mises à jour

--- app/assets/js/scripts/settings.js
+++ app/assets/js/scripts/settings.js
@@ -1453,7 +1453,7 @@ function populateAboutVersionInformation(){
  */
 function populateReleaseNotes(){
     $.ajax({
-        url: 'https://github.com/dscalzi/HeliosLauncher/releases.atom',
+        url: 'https://github.com/dremixam/BigzbeulLauncher/releases.atom',
         success: (data) => {
             const version = 'v' + remote.app.getVersion()
             const entries = $(data).find('entry')
