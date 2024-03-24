4fd34dc5490dd4c40aeab980452b87622896a168
Shadrin Igor <igor@shadrins.ru>
2024-03-24 16:11:54 +0300

    path fix

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'https://creatopico.ru/distribution.json'
+exports.REMOTE_DISTRO_URL = 'https://creatopico.ru/distribution/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
