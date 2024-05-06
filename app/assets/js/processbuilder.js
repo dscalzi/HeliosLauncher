22102da217e4e5b9bf7354fd5d0dee20d2a6c1c4
LegendMC <ulysse.gresselugue@gmail.com>
2024-05-06 11:10:57 +0200

    Close the launcher on game launch.
    Added a requested feature in: #246
    
    Default: false

--- app/assets/js/processbuilder.js
+++ app/assets/js/processbuilder.js
@@ -7,6 +7,8 @@ const { getMojangOS, isLibraryCompatible, mcVersionAtLeast }  = require('helios-
 const { Type }              = require('helios-distribution-types')
 const os                    = require('os')
 const path                  = require('path')
+const remote = require('@electron/remote');
+const win = remote.getCurrentWindow();
 
 const ConfigManager            = require('./configmanager')
 
@@ -80,6 +82,10 @@ class ProcessBuilder {
 
         if(ConfigManager.getLaunchDetached()){
             child.unref()
+        }
+
+        if(ConfigManager.getCloseOnLaunch()){
+            win.close();
         }
 
         child.stdout.setEncoding('utf8')
