7d0a572fad9673d96889bdda642df65f710f0002
Leafd <leafd@marcusk.fun>
2024-10-02 21:50:05 -0600

    feat(auth): change azure auth client id

--- app/assets/js/ipcconstants.js
+++ app/assets/js/ipcconstants.js
@@ -1,7 +1,7 @@
 // NOTE FOR THIRD-PARTY
 // REPLACE THIS CLIENT ID WITH YOUR APPLICATION ID.
 // SEE https://github.com/dscalzi/HeliosLauncher/blob/master/docs/MicrosoftAuth.md
-exports.AZURE_CLIENT_ID = '1ce6e35a-126f-48fd-97fb-54d143ac6d45'
+exports.AZURE_CLIENT_ID = 'aed16161-d55a-4dea-a489-9b9b288ae27d'
 // SEE NOTE ABOVE.
 
 
