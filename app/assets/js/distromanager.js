bab8b3059f2a75a0e8b68350080932209cb31797
RedSavant <jjumper999@gmail.com>
2025-11-09 15:47:24 +0100

    GE

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'https://helios-files.geekcorner.eu.org/distribution.json'
+exports.REMOTE_DISTRO_URL = 'http://37.59.126.77:8080/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
