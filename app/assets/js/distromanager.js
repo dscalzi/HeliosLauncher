e76fca1912e7ee9214a8067a04e5e1c39ec00107
RenanPlay13013 <renanluisdepaula13@outlook.com>
2025-12-30 01:26:51 -0300

    updated REMOTE_DISTRO_URL

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'http://launcher.loyalnetwork.com.br/www/launcher/distribution.json'
+exports.REMOTE_DISTRO_URL = 'http://178.132.198.229:21163/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
